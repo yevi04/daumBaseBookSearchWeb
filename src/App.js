eee3f48e12446f1128cc89c243b4c1aa5e512bc9
YoungJun-Kim <87963601+kyjprograming@users.noreply.github.com>
2022-12-23 14:29:33 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+
   return (
     <div
       style={{
@@ -13,10 +14,11 @@ const App = () => {
         height: window.innerHeight,
       }}
     >
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', marginBottom: 25, width: '80%', justifyContent: 'center'}}>
         <input
           type='text'
-          style={{ width: 400, height: 40, marginRight: 15 }}
+          placeholder='검색어를 입력하세요'
+          style={{ width: '50%', height: 40, marginRight: 15, padding: 10, outline: 'none', borderRadius: 5, fontSize:15, fontWeight: 'bold', paddingLeft: 25}}
           id='input'
         />
         <input
@@ -28,7 +30,7 @@ const App = () => {
             )
           }
           value='검색'
-          style={{ width: 85, height: 45, textAlign: 'center' }}
+          style={{ width: 85, height: 65, textAlign: 'center' }}
         />
       </div>
       {
@@ -80,7 +82,7 @@ const KakaoSearchApi = (root, options) => {
     document.getElementById('input').value
   )}${options}`;
 
-  let data = fetch(url, {
+  let data = document.getElementById('input').value !== '' ? fetch(url, {
     method: 'get',
     headers: {
       'Content-Type': 'application/json',
@@ -92,22 +94,24 @@ const KakaoSearchApi = (root, options) => {
       alert('검색 성공!!');
       if (window.confirm('정보를 불러오시겠습니까?')) {
         for (let i = 0; i < 10; i++) {
-          console.log(jsonString.documents[i])
+          console.log(jsonString.documents[i].thumbnail)
+          const thumbnail = jsonString.documents[i].thumbnail !== '' ? `${jsonString.documents[i].thumbnail}` : 'https://search1.kakaocdn.net/thumb/C116x164.q85/?fname=http://search1.daumcdn.net/search/statics/common/img/noimg/4grid.png'
+          const sale_price = jsonString.documents[i].sale_price === -1 ? '재고없음' : jsonString.documents[i].sale_price.toString() + '원'
           document.getElementById('info').innerHTML += `<a href='${
             jsonString.documents[i].url
           }'>
-            <div class='infoText'>
-              <img src='${jsonString.documents[i].thumbnail}' class='test'/>
-              <div>
-              책 이름 : ${jsonString.documents[i].title}<br>
-              가격 : ${jsonString.documents[i].price.toString()}<br>
+            <div class='infoTemplate'>
+              <img src=${thumbnail} class='infoImage'/>
+              <div class='infoText'>
+                <span class='title'>${jsonString.documents[i].title}</span><br>
+                판매가: ${sale_price}<br>
               </div>
             </div>
           </a>`;
         }
       }
     });
-  });
+  }) : alert('검색어를 입력해주세요');
 };
 
 export default App;
