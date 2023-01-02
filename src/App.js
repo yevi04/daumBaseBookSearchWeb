b0c4b1fc1f77396ff994046cd7e7781bbc00276c
YoungJun-Kim <yjkim5908@naver.com>
2023-01-02 10:27:12 +0900

    book search api study among daum search api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-
 const App = () => {
-
   return (
     <div
       style={{
@@ -33,45 +31,8 @@ const App = () => {
           style={{ width: 85, height: 65, textAlign: 'center' }}
         />
       </div>
-      {
-        /**
-         * 
-         * 
-         * <div>
-        <button
-          onClick={() => setSort('accuracy')}
-          style={
-            sort === 'accuracy'
-              ? {
-                  backgroundColor: 'black',
-                  color: 'white',
-                  marginRight: 10,
-                  marginTop: 10,
-                }
-              : {
-                  backgroundColor: 'white',
-                  color: 'black',
-                  marginRight: 10,
-                  marginTop: 10,
-                }
-          }
-        >
-          정확도순
-        </button>
-        <button
-          onClick={() => setSort('latest')}
-          style={
-            sort === 'latest'
-              ? { backgroundColor: 'black', color: 'white' }
-              : { backgroundColor: 'white', color: 'black' }
-          }
-        >
-          발간일순
-        </button>
-      </div>
-         */
-      }
       <div id='info'>
+        
       </div>
     </div>
   );
@@ -86,30 +47,56 @@ const KakaoSearchApi = (root, options) => {
     method: 'get',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: 'KakaoAK b1f8f739bb057a196a0c20ba4806b15e',
+      'Authorization': 'KakaoAK b1f8f739bb057a196a0c20ba4806b15e',
     },
   }).then((response) => {
     response.text().then((res) => {
       const jsonString = JSON.parse(res.toString());
-      alert('검색 성공!!');
-      if (window.confirm('정보를 불러오시겠습니까?')) {
-        for (let i = 0; i < 10; i++) {
-          console.log(jsonString.documents[i].thumbnail)
-          const thumbnail = jsonString.documents[i].thumbnail !== '' ? `${jsonString.documents[i].thumbnail}` : 'https://search1.kakaocdn.net/thumb/C116x164.q85/?fname=http://search1.daumcdn.net/search/statics/common/img/noimg/4grid.png'
-          const sale_price = jsonString.documents[i].sale_price === -1 ? '재고없음' : jsonString.documents[i].sale_price.toString() + '원'
-          document.getElementById('info').innerHTML += `<a href='${
-            jsonString.documents[i].url
-          }'>
-            <div class='infoTemplate'>
-              <img src=${thumbnail} class='infoImage'/>
-              <div class='infoText'>
-                <span class='title'>${jsonString.documents[i].title}</span><br>
-                판매가: ${sale_price}<br>
-              </div>
-            </div>
-          </a>`;
+      if (window.confirm('판매 중인 책의 정보를 불러오시겠습니까?')) {
+        document.getElementById('info').innerHTML = ''
+        if(jsonString.documents.length > 0){
+          for (let i = 0; i < 10; i++) {
+            const authors = jsonString.documents[i].authors[0]
+            const url = jsonString.documents[i].url
+            const title = jsonString.documents[i].title
+            const datetime = jsonString.documents[i].datetime.substring(0, 10).split('-')
+            const publisher = jsonString.documents[i].publisher
+            const thumbnail = jsonString.documents[i].thumbnail !== '' ? `${jsonString.documents[i].thumbnail}` : 'https://search1.kakaocdn.net/thumb/C116x164.q85/?fname=http://search1.daumcdn.net/search/statics/common/img/noimg/4grid.png'
+            const sale_price = jsonString.documents[i].sale_price === -1 ? '' : '판매가: ' + jsonString.documents[i].sale_price.toString() + '원<br>'
+            const status = jsonString.documents[i].sale_price === -1 ? '품절 혹은 절판' : jsonString.documents[i].status
+            if(status != '품절 혹은 절판'){
+              document.getElementById('info').innerHTML += 
+            `
+              <a href=${url}>
+                <div class='infoTemplate'>
+                  <img src=${thumbnail} class='infoImage'/>
+                  <div class='infoText'>
+                    <span class='title'>${title}</span><br>
+                    대표 저자: ${authors}<br>
+                    출판: ${publisher}</a> | ${datetime[0]}년${datetime[1]}월${datetime[2]}일<br>
+                    ${sale_price}
+                    판매 상태: ${status}
+                  </div>
+                </div>
+              </a>
+            `;
+            }
+          };
         }
-      }
+        if(document.getElementById('info').innerHTML == ''){
+          document.getElementById('info').innerHTML += 
+        `
+            <div class='this_keyword_book_not_sale'>
+              판매중인 책이 없습니다.
+            </div>
+        `;
+        } else {
+            if(window.confirm('가장 맨 하단 정보로 스크롤 하시겠습니까?')){
+              document.documentElement.scrollTop = document.documentElement.scrollHeight;
+            }
+        }
+        
+      };
     });
   }) : alert('검색어를 입력해주세요');
 };
